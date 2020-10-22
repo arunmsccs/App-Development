af0520518971cf8ddd8454d65b925f6f64f781b4
Arun Vediyappan <arunmsccs@gmail.com>
2020-10-23 00:08:39 +0530

    Included component Libraryx for Header Footer Card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Header, Card, Footer } from 'component-libraryx'
+import 'component-libraryx/dist/index.css'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Card name="Fruits" price="99" />
+      <Footer />
     </div>
   );
 }
